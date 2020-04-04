35ed7ce3f3789ecc0c691c1e64f97c069b1ef957
viu <vishnu.kraman88@gmail.com>
2020-04-04 23:24:41 +0530

    parsing element data

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,18 @@ class ToolTip {}
 
 class ProjectItem {}
 
-class ProjectList {}
+class ProjectList {
+  constructor(type) {
+    const projectItem = document.querySelectorAll(`#${type}-projects li`);
+    console.log(projectItem);
+  }
+}
 
-class App {}
+class App {
+  static init() {
+    const activeProjectsList = new ProjectList("active");
+    const finishedProjectsList = new ProjectList("finished");
+  }
+}
+
+App.init();
